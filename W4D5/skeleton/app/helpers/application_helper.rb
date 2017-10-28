12a33efaf45e2d8a91f5d773c0bb89e7f3a4b88e
JiwooPark <jpark830@me.com>
2017-10-27 21:31:44 -0700

    W5D1

---
+++ W4D5/skeleton/app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+  def auth_token
+      html_str = '<input type="hidden" name="authenticity_token" value="<%= form_authenticity_token %>"/>'
+
+      html_str.html_safe
+  end
+
+end
