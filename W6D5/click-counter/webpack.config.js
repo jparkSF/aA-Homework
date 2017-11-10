7fcc9ebecd8e690da11a317776d57b35d82dd0cb
JiwooPark <jpark830@me.com>
2017-11-10 05:06:19 -0800

    W6D5

---
+++ W6D5/click-counter/webpack.config.js
@@ -0,0 +1,27 @@
+let path = require('path');
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./entry.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx" ]
+  }
+};
